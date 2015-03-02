ae3bdbf504bd394c719a333f9f9e03d1d3892752
mcgr0217 <Samantha@samanthas-MacBook-Pro.local>
2015-03-02 17:57:38 -0500

    Adding the vars
    Adding the vars to the JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var $btn-show-hide = $(".btn-show-hide");
+var $box = $(".box");
+
+var $btn-move = $(".btn-move");
+var $diamond = $(".diamond");
+
+var $btn-collapse-expand = $(".btn-collapse-expand");
+var $panel = $(".panel");
+
+var $btn-bounce = $(".btn-bounce");
+var $circle = $(".circle");
+
+var $btn-append = $(".btn-append");
+var $list = $(".list");
