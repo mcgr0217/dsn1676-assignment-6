8aeb3a620fd0bf35c7c301e5b10ac7fc78bbf535
mcgr0217 <Samantha@samanthas-MacBook-Pro.local>
2015-03-03 13:17:46 -0500

    Trying to fix the panel collapse
    Trying to change the naming as well as the transitions but still not
    working

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $btnMove.on("click", function () {
 });
 
 $btnCollapse.on("click", function () {
-    $panel.toggleClass("js-panel-expand");
+    $panel.toggleClass("js-panel-collapse");
 });
 
 $btnBounce.on("click", function () {
@@ -34,7 +34,7 @@ $circle.on("animationend", function () {
 });
 
 $btnAppend.on("click", function () {
-    var $li = $('<li>').html("hello");
+    var $li = $("<li>").html("hello");
     $li.addClass("js-list-append");
     $list.append($li);
 });
